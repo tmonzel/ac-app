0b2b3272ce18d4206ece394838d0bf3ee14050fd
Thomas Monzel <t.monzel@gds-gmbh.de>
2024-10-15 08:21:17 +0200

    Add final dsgvo

--- src/routes/datenschutz/+page.svelte
+++ src/routes/datenschutz/+page.svelte
@@ -10,37 +10,348 @@
 
 <div class="container mx-auto py-10 px-2 sm:px-0">
 	<header class="text-center">
-		<h1 class="text-5xl font-bold tracking-tight hl-font">Datenschutz</h1>
+		<h1 class="text-5xl font-bold tracking-tight hl-font">Datenschutzerklärung</h1>
 	</header>
 
 	<hr class="my-10" />
 
 	<div class="px-2 sm:px-20">
 		<section class="mb-10">
-			<h2 class="font-semibold mb-5">Verantwortlicher für die Datenverarbeitung ist:</h2>
 			<p class="mb-10">
-				Azubi Companion gGmbH<br />
-				Budnikowsky-Twiete 7<br />
-				22041 Hamburg<br />
-				post@azubi-companion.de
-			</p>
-
-			<p>
-				Geschäftsführende Gesellschafterin:<br />Dr. Melanie Steinhoff<br /><br />
-				Geschäftsführende Gesellschafterin:<br />Julia Wöhlke
+				Wir freuen uns über Ihr Interesse an unserer Website und den Angeboten der Azubi Companion
+				gGmbH. <br />Natürlich ist uns auch der Schutz Ihrer personenbezogenen Daten anlässlich
+				Ihres Besuchs unserer Internetseiten ein wichtiges Anliegen.
 			</p>
 		</section>
 
 		<hr class="my-10 w-10" />
 
-		<section>
-			<h2 class="font-semibold mb-5">Zugriffsdaten und Hosting</h2>
+		<section class="mb-8">
+			<h2 class="font-semibold mb-5">1. Allgemeines zur Datenverarbeitung</h2>
+			<p class="mb-5">
+				Mit den folgenden Datenschutzhinweisen informieren wir Sie über die Art und den Umfang der
+				Verarbeitung personenbezogener Daten. Wir erheben und verwenden personenbezogene Daten der
+				Besucher unserer Internetseite nur, soweit dies erforderlich ist, um die Internetseite sowie
+				unsere Inhalte und Leistungen funktionsfähig bereitzustellen und für die Besucher
+				komfortabel nutzbar zu machen.
+			</p>
+
+			<p class="mb-5">
+				<strong>„Personenbezogene Daten“</strong> sind gemäß Art. 4 Nr. 1 DSGVO alle Informationen, die
+				sich auf eine identifizierte oder identifizierbare natürliche Person („betroffene Person”) beziehen.
+				Dazu zählen z.B. der Name, die Anschrift, das Geburtsdatum, die E-Mail-Adresse und die Telefonnummer.
+			</p>
+
+			<p class="mb-5">
+				<strong>„Verarbeitung”</strong> meint gemäß Art. 4 Nr. 2 DSGVO jeden mit oder ohne Hilfe automatisierter
+				Verfahren ausgeführten Vorgang oder jede solche Vorgangsreihe im Zusammenhang mit personenbezogenen
+				Daten (z.B. die Erhebung, Speicherung, Verwendung oder Offenlegung).
+			</p>
+
+			<p class="mb-5">
+				<strong>„Verantwortlicher”</strong> ist nach Art. 4 Nr. 7 DSGVO die natürliche oder juristische
+				Person, Behörde, Einrichtung oder andere Stelle, die allein oder gemeinsam mit anderen über die
+				Zwecke und Mittel der Verarbeitung von personenbezogenen Daten entscheidet.
+			</p>
+
+			<p class="mb-5">
+				Die personenbezogenen Daten der betroffenen Person werden gelöscht oder gesperrt, sobald der
+				Zweck der Speicherung entfällt. Eine Speicherung kann darüber hinaus erfolgen, wenn dies
+				durch den europäischen oder nationalen Gesetzgeber in unionsrechtlichen Verordnungen,
+				Gesetzen oder sonstigen Vorschriften, denen der Verantwortliche unterliegt, vorgesehen
+				wurde. Eine Sperrung oder Löschung der Daten erfolgt auch dann, wenn eine durch die
+				genannten Normen vorgeschriebene Speicherfrist abläuft, es sei denn, dass eine
+				Erforderlichkeit zur weiteren Speicherung der Daten für einen Vertragsabschluss oder eine
+				Vertragserfüllung besteht.
+			</p>
+		</section>
+
+		<section class="mb-8">
+			<h2 class="font-semibold mb-5">
+				2. Namen und Kontaktdaten des Verantwortlichen, Kontaktdaten des/der Datenschutzbeauftragten
+			</h2>
+			<p class="mb-5">
+				Verantwortlicher für die Datenverarbeitung beim Besuch und der Nutzung dieser Internetseite
+				ist:<br /><br />Azubi Companion gGmbH <br />Budnikowsky-Twiete 7 <br />22041 Hamburg
+			</p>
+
 			<p>
-				Sie können unsere Webseiten besuchen, ohne Angaben zu Ihrer Person zu machen. Bei jedem
-				Aufruf einer Webseite speichert der Webserver lediglich automatisch ein sogenanntes
-				Server-Logfile, das z.B. den Namen der angeforderten Datei, Ihre IP-Adresse, Datum und
-				Uhrzeit des Abrufs, übertragene Datenmenge und den anfragenden Provider (Zugriffsdaten)
-				enthält und den Abruf dokumentiert.
+				vertreten durch die Geschäftsführerinnen Dr. Melanie Steinhoff und Julia Wöhlke<br /> Tel:
+				49 (0)178/ 5333416<br />E-Mail: post@azubi-companion.de
+			</p>
+		</section>
+
+		<section class="mb-8">
+			<h2 class="font-semibold mb-5">
+				3. Verarbeitung von Daten beim Besuch und der Nutzung unserer Internetseite
+			</h2>
+
+			<h3>3.1 Speicherung von Zugriffsdaten in http-Protokolldateien („log files")</h3>
+
+			<p class="mb-5">
+				Sie können unsere Internetseite grundsätzlich besuchen und nutzen, ohne Angaben zu Ihrer
+				Person zu machen. Beim Besuch unserer Internetseite übermittelt der von Ihnen genutzte
+				Internet-Browser automatisch bestimmte Zugriffsdaten an den bereitgestellten Server. Dabei
+				handelt es sich um folgende Daten:
+			</p>
+
+			<ul class="mb-5 ps-5">
+				<li>IP-Adresse des anfragenden Rechners</li>
+				<li>verwendete Browsersoftware sowie deren Version und Sprache</li>
+				<li>Internet-Service-Provider des Nutzers</li>
+				<li>Betriebssystem des anfragenden Rechners</li>
+				<li>Datum und Uhrzeit des Zugriffs</li>
+				<li>Name und URL der abgerufenen Seite bzw. Datei</li>
+				<li>Internetseite, von der aus der Zugriff erfolgt</li>
+				<li>
+					Internetseite, die vom Systems des Nutzers über unsere Internetseite aufgerufen wurde.
+				</li>
+				<li>Zugriffsstatus/http-Statuscode</li>
+				<li>jeweils übertragene Datenmenge</li>
+			</ul>
+
+			<p class="mb-5">
+				<span class="underline">Art und Zwecke der Datenverarbeitung:</span><br />
+				Diese Daten werden temporär in einer eigenen Protokolldatei (sog. „log file") gespeichert. Die
+				Logfiles enthalten IP-Adressen oder sonstige Daten, die eine Zuordnung zu einem Nutzer ermöglichen.
+				Dies könnte beispielsweise der Fall sein, wenn der Link zur Website, von der der Nutzer auf die
+				Internetseite gelangt, oder der Link zur Website, zu der der Nutzer wechselt, personenbezogene
+				Daten enthält.
+			</p>
+
+			<p class="mb-5">
+				Die Speicherung dient dem Zweck, die Systemsicherheit und -stabilität dauerhaft zu
+				gewährleisten und die technische Administration zu ermöglichen, um somit einen
+				störungsfreien Verbindungsaufbau und Betrieb unserer Internetseite sowie deren komfortable
+				Nutzung sicherzustellen. Zudem werden diese Daten zu internen administrativen und
+				statistischen Zwecken ausgewertet, um unser Online-Angebot zu verbessern. Eine
+				Zusammenführung dieser Daten mit anderen Daten bzw. Datenquellen, die Rückschlüsse auf Ihre
+				Person ermöglichen würde, wird nicht vorgenommen.
+			</p>
+
+			<p class="mb-5">
+				<span class="underline">Rechtsgrundlage:</span><br />
+				Rechtsgrundlage für diese Verarbeitung ist Art. 6 Abs. 1 lit. f DSGVO. Wir verfolgen mit dieser
+				Datenverarbeitung das berechtigte Interesse, die Betriebssicherheit unserer Internetseite aufrechtzuerhalten,
+				um diese Internetseite sowie die dort enthaltenen Informationen störungsfrei und komfortabel
+				bereitstellen zu können.
+			</p>
+
+			<p class="mb-5">
+				<span class="underline">Speicherdauer:</span><br />
+				Die Daten werden gelöscht, sobald sie für die Erreichung der vorgenannten Zwecke ihrer Erhebung
+				nicht mehr erforderlich sind. Im Falle der Speicherung der Daten in Logfiles ist dies nach spätestens
+				sieben Tagen der Fall. Eine darüberhinausgehende Speicherung ist möglich. In diesem Fall werden
+				die IP-Adressen der Nutzer gelöscht oder verfremdet, sodass eine Zuordnung des aufrufenden Clients
+				nicht mehr möglich ist.
+			</p>
+
+			<p class="mb-5">
+				<span class="underline">Widerrufs- und Beseitigungsmöglichkeit:</span><br />
+				Die Erfassung der Daten zur Bereitstellung der Website und die Speicherung der Daten in Logfiles
+				ist für den Betrieb der Internetseite zwingend erforderlich. Es besteht folglich seitens des
+				Nutzers keine Widerspruchsmöglichkeit.
+			</p>
+
+			<h3 class="font-semibold">3.2 Cookies und Webanalyse</h3>
+
+			<p class="mb-5">
+				Wir verwenden keine Cookies oder Google Analytics oder ähnliche Tracking- und
+				Webanalysetools.
+			</p>
+
+			<h3 class="font-semibold">
+				3.3 Gesetzliche oder vertragliche Verpflichtung zur Bereitstellung der Daten
+			</h3>
+			<p class="mb-5">
+				Sofern Sie uns eine Kontaktanfrage senden und möchten, dass wir Ihre Anfrage entsprechend
+				bearbeiten und beantworten, benötigen wir die dort als erforderlich angegebenen
+				personenbezogenen Daten. Andernfalls ist uns die Bearbeitung der Anfrage nicht möglich.
+				Sofern Sie sich für unseren Webshop registrieren möchten, benötigen wir ebenfalls die dort
+				als erforderlich angegebenen Daten, um die Registrierung durchführen zu können. Ohne diese
+				erforderlichen personenbezogenen Daten können wir diese Vorgänge nicht durchführen und den
+				Vertrag nicht ordnungsgemäß mit Ihnen abschließen.
+			</p>
+
+			<h3 class="font-semibold">3.4 Datensicherheit</h3>
+			<p class="mb-5">
+				Wir sind bemüht, die Sicherheit Ihrer Daten zu gewährleisten. Um den Verlust, den Missbrauch
+				und die Veränderung personenbezogener Daten zu verhindern, haben wir entsprechende
+				physikalische, elektronische und verwaltungstechnische Verfahren eingerichtet und passen
+				diese dem aktuellen Stand der Technik entsprechend an.
+			</p>
+			<p class="mb-5">
+				Diese Seite nutzt aus Gründen der Sicherheit und zum Schutz der Übertragung vertraulicher
+				Inhalte, wie zum Beispiel der Anfragen, die Sie an uns als Seitenbetreiber senden, eine
+				SSL-Verschlüsselung. Eine verschlüsselte Verbindung erkennen Sie daran, dass die Adresszeile
+				des Browsers von "http://" auf "https://" wechselt und an dem Schloss-Symbol in Ihrer
+				Browserzeile. Wenn die SSL Verschlüsselung aktiviert ist, können die Daten, die Sie an uns
+				übermitteln, nicht von Dritten mitgelesen werden.
+			</p>
+
+			<h3 class="font-semibold">
+				3.5 Kein Einsatz automatisierter Entscheidungsfindung einschließlich Profiling
+			</h3>
+			<p class="mb-5">
+				Wir setzen weder ein sog. Profiling noch sonstige Entscheidungsfindungen ein, die
+				ausschließlich auf einer automatisierten Datenverarbeitung beruhen und Ihnen gegenüber
+				rechtliche Wirkung entfalten oder Sie in ähnlicher Weise erheblich beeinträchtigen.
+			</p>
+		</section>
+
+		<section>
+			<h2 class="font-semibold mb-5">4. Ihre Betroffenenrechte</h2>
+
+			<p class="mb-5">
+				Sie haben bezüglich der Verarbeitung Ihrer personenbezogenen Daten die folgenden Rechte:
+			</p>
+
+			<h3 class="font-semibold">4.1 Recht auf Auskunft (Art. 15 DSGVO)</h3>
+			<p class="mb-5">
+				Sie haben das Recht, Auskunft über Ihre von uns verarbeiteten personenbezogenen Daten zu
+				verlangen. Insbesondere können Sie Auskunft über die Verarbeitungszwecke, die Kategorien der
+				personenbezogenen Daten, die Kategorien von Empfängern, gegenüber denen Ihre Daten
+				offengelegt wurden oder werden, die geplante Speicherdauer, das Bestehen eines Rechts auf
+				Berichtigung, Löschung, Einschränkung der Verarbeitung oder Widerspruch, das Bestehen eines
+				Beschwerderechts, die Herkunft ihrer Daten, sofern diese nicht bei uns erhoben wurden,
+				verlangen.
+			</p>
+
+			<p class="mb-5">
+				Ihnen steht ferner das Recht zu, Auskunft darüber zu verlangen, ob die Sie betreffenden
+				personenbezogenen Daten in ein Drittland oder an eine internationale Organisation
+				übermittelt werden. In diesem Zusammenhang können Sie verlangen, über die geeigneten
+				Garantien gem. Art. 46 DSGVO im Zusammenhang mit der Übermittlung unterrichtet zu werden.
+			</p>
+
+			<h3 class="font-semibold">4.2 Recht auf Berichtigung (Art. 16 DSGVO)</h3>
+			<p class="mb-5">
+				Sie haben das Recht, unverzüglich die Berichtigung von Daten zu Ihrer Person zu verlangen,
+				die bei uns unrichtig gespeichert sind, oder deren Vervollständigung zu verlangen, soweit
+				sie bei uns unvollständig gespeichert sind.
+			</p>
+
+			<h3 class="font-semibold">4.3 Recht auf Löschung (Art. 17 DSGVO)</h3>
+			<p class="mb-5">
+				Sie haben das Recht, die Löschung Ihrer bei uns gespeicherten personenbezogenen Daten zu
+				verlangen, sofern einer der folgenden Gründe zutrifft:
+			</p>
+
+			<ul class="mb-5 ps-5">
+				<li>
+					Die Sie betreffenden personenbezogenen Daten sind für die Zwecke, für die sie erhoben oder
+					auf sonstige Weise verarbeitet wurden, nicht mehr notwendig.
+				</li>
+				<li>
+					Sie widerrufen Ihre Einwilligung, auf die sich die Verarbeitung gem. Art. 6 Abs. 1 lit. a
+					oder Art. 9 Abs. 2 lit. a DSGVO stützte, und es fehlt an einer anderweitigen
+					Rechtsgrundlage für die Verarbeitung.
+				</li>
+				<li>
+					Sie legen gem. Art. 21 Abs. 1 DSGVO Widerspruch gegen die Verarbeitung ein und es liegen
+					keine vorrangigen berechtigten Gründe für die Verarbeitung vor, oder Sie legen gem. Art.
+					21 Abs. 2 DSGVO Widerspruch gegen die Verarbeitung ein.
+				</li>
+				<li>Die Sie betreffenden personenbezogenen Daten wurden unrechtmäßig verarbeitet.</li>
+				<li>
+					Die Löschung der Sie betreffenden personenbezogenen Daten ist zur Erfüllung einer
+					rechtlichen Verpflichtung nach dem Unionsrecht oder dem Recht der Mitgliedstaaten
+					erforderlich, dem der Verantwortliche unterliegt.
+				</li>
+				<li>
+					Die Sie betreffenden personenbezogenen Daten wurden in Bezug auf angebotene Dienste der
+					Informationsgesellschaft gemäß Art. 8 Abs. 1 DSGVO erhoben.
+				</li>
+			</ul>
+
+			<p class="mb-5">
+				Hat der Verantwortliche die Sie betreffenden personenbezogenen Daten öffentlich gemacht und
+				ist er gem. Art. 17 Abs. 1 DSGVO zu deren Löschung verpflichtet, so trifft er unter
+				Berücksichtigung der verfügbaren Technologie und der Implementierungskosten angemessene
+				Maßnahmen, auch technischer Art, um für die Datenverarbeitung Verantwortliche, die die
+				personenbezogenen Daten verarbeiten, darüber zu informieren, dass Sie als betroffene Person
+				von ihnen die Löschung aller Links zu diesen personenbezogenen Daten oder von Kopien oder
+				Replikationen dieser personenbezogenen Daten verlangt haben.
+			</p>
+
+			<p class="mb-5">
+				Das Recht auf Löschung besteht nicht, soweit die Verarbeitung erforderlich ist
+			</p>
+
+			<ul class="ps-5 mb-5">
+				<li>zur Ausübung des Rechts auf freie Meinungsäußerung und Information;</li>
+				<li>
+					zur Erfüllung einer rechtlichen Verpflichtung, die die Verarbeitung nach dem Recht der
+					Union oder der Mitgliedstaaten, dem der Verantwortliche unterliegt, erfordert, oder zur
+					Wahrnehmung einer Aufgabe, die im öffentlichen Interesse liegt oder in Ausübung
+					öffentlicher Gewalt erfolgt, die dem Verantwortlichen übertragen wurde;
+				</li>
+				<li>
+					aus Gründen des öffentlichen Interesses im Bereich der öffentlichen Gesundheit gemäß Art.
+					9 Abs. 2 lit. h und i sowie Art. 9 Abs. 3 DSGVO;
+				</li>
+				<li>
+					für im öffentlichen Interesse liegende Archivzwecke, wissenschaftliche oder historische
+					Forschungszwecke oder für statistische Zwecke gem. Art. 89 Abs. 1 DSGVO, soweit das
+					genannte Recht voraussichtlich die Verwirklichung der Ziele dieser Verarbeitung unmöglich
+					macht oder ernsthaft beeinträchtigt, oder
+				</li>
+				<li>zur Geltendmachung, Ausübung oder Verteidigung von Rechtsansprüchen.</li>
+			</ul>
+
+			<h3 class="font-semibold">4.4 Recht auf Einschränkung der Verarbeitung (Art. 18 DSGVO)</h3>
+			<p class="mb-5">
+				Sie haben das Recht, die Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu
+				verlangen, soweit die Richtigkeit der Daten von Ihnen bestritten wird, soweit die
+				Verarbeitung unrechtmäßig ist, Sie aber die Löschung der Daten ablehnen, soweit wir die
+				Daten nicht mehr benötigen, Sie diese jedoch zur Geltendmachung, Ausübung oder Verteidigung
+				von Rechtsansprüchen benötigen, oder soweit Sie gemäß Art. 21 DSGVO Widerspruch gegen die
+				Verarbeitung eingelegt haben.
+			</p>
+
+			<h3 class="font-semibold">4.5 Recht auf Unterrichtung (Art. 19 DSGVO)</h3>
+			<p class="mb-5">
+				Haben Sie das Recht auf Berichtigung, Löschung oder Einschränkung der Verarbeitung gegenüber
+				dem Verantwortlichen geltend gemacht, ist dieser verpflichtet, allen Empfängern, denen die
+				Sie betreffenden personenbezogenen Daten offengelegt wurden, diese Berichtigung oder
+				Löschung der Daten oder Einschränkung der Verarbeitung mitzuteilen, es sei denn, dies
+				erweist sich als unmöglich oder ist mit einem unverhältnismäßigen Aufwand verbunden.
+			</p>
+
+			<h3 class="font-semibold">4.6 Recht auf Datenübertragbarkeit (Art. 20 DSGVO)</h3>
+			<p class="mb-5">
+				Sie haben das Recht, Ihre personenbezogenen Daten, die Sie uns bereitgestellt haben, in
+				einem strukturierten, gängigen und maschinenlesebaren Format zu erhalten oder die
+				Übermittlung an einen anderen Verantwortlichen zu verlangen.
+			</p>
+
+			<h3 class="font-semibold">
+				4.7 Recht auf jederzeitigen Widerruf einer erteilten Einwilligung (Art. 7 Abs. 3 i.V.m. Art.
+				6 Abs. 1Buchst. a bzw. Art. 9 Abs. 2 Buchst. a DSGVO)
+			</h3>
+			<p class="mb-5">
+				Sie haben das Recht, eine erteilte Einwilligung jederzeit gegenüber uns zu widerrufen. Dies
+				hat zur Folge, dass wir die Datenverarbeitung, die auf dieser Einwilligung beruhte, für die
+				Zukunft nicht mehr fortführen dürfen, sofern diese nicht auf eine andere Rechtsgrundlage
+				gestützt werden kann.
+			</p>
+
+			<h3 class="font-semibold">
+				4.8 Beschwerderecht bei einer Aufsichtsbehörde (Art. 77 DSGVO i.V.m. § 19 BDSG 2018)
+			</h3>
+			<p class="mb-5">
+				Sie haben das Recht, sich bei einer Aufsichtsbehörde zu beschweren, wenn Sie der Ansicht
+				sind, dass die Verarbeitung Ihrer personenbezogenen Daten gegen die DSGVO verstößt. In der
+				Regel können Sie sich hierfür an die Aufsichtsbehörde Ihres üblichen Aufenthaltsortes oder
+				Arbeitsplatzes oder unseres Firmensitzes wenden.
+			</p>
+
+			<h3 class="font-semibold">4.9 Widerspruchsrecht (Art. 21 DSGVO)</h3>
+			<p class="mb-5">
+				Wenn wir Daten zu Ihrer Person aufgrund berechtigter Interessen verarbeiten, können Sie dem
+				aus Gründen widersprechen, die sich aus Ihrer besonderen Situation ergeben. Außerdem können
+				Sie einer Datenverarbeitung widersprechen, wenn wir diese zu Zwecken der Direktwerbung
+				vornehmen.
 			</p>
 		</section>
 	</div>
