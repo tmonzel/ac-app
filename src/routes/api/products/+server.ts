3947e00ae06fd765af7e55b17ed0984012829ddc
Thomas Monzel <t.monzel@gds-gmbh.de>
2025-03-04 08:39:35 +0100

    Catch and return api/products error

--- src/routes/api/products/+server.ts
+++ src/routes/api/products/+server.ts
@@ -1,9 +1,16 @@
 import { stripe } from '$lib/server/stripe';
 import type { RequestHandler } from '@sveltejs/kit';
 import { json } from '@sveltejs/kit';
+import type Stripe from 'stripe';
 
 export const GET: RequestHandler = async () => {
-	const products = await stripe.products.list();
+	let products: Stripe.Response<Stripe.ApiList<Stripe.Product>>;
+
+	try {
+		products = await stripe.products.list();
+	} catch (err) {
+		return json(err);
+	}
 
 	return json(products);
 };
