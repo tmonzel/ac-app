a3e6d1a31165fa69358e572a2fc592bd45b81bd3
Thomas Monzel <t.monzel@gds-gmbh.de>
2025-03-04 09:05:56 +0100

    Install stripe as devDep

--- src/routes/api/products/+server.ts
+++ src/routes/api/products/+server.ts
@@ -1,16 +1,9 @@
 import { stripe } from '$lib/server/stripe';
 import type { RequestHandler } from '@sveltejs/kit';
 import { json } from '@sveltejs/kit';
-import type Stripe from 'stripe';
 
 export const GET: RequestHandler = async () => {
-	let products: Stripe.Response<Stripe.ApiList<Stripe.Product>>;
-
-	try {
-		products = await stripe.products.list();
-	} catch (err) {
-		return json(err);
-	}
+	const products = await stripe.products.list();
 
 	return json(products);
 };
