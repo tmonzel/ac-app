b5775d9976f0b72e1df7c510e0b74457afd98227
Thomas Monzel <t.monzel@gds-gmbh.de>
2025-03-06 12:14:17 +0100

    Change amplify configuration

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,9 @@
 import type { Handle } from '@sveltejs/kit';
 import { Amplify } from 'aws-amplify';
 import outputs from '../amplify_outputs.json';
-import config from '../aws-exports';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	Amplify.configure({ ...outputs, ...config });
+	Amplify.configure(outputs);
 
 	const response = await resolve(event);
 	return response;
