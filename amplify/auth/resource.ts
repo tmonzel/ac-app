8bee69ff5bbedb8406292dad489c90f98b906a8b
Thomas Monzel <t.monzel@gds-gmbh.de>
2025-03-06 08:51:11 +0100

    Remove amplify backend

--- amplify/auth/resource.ts
+++
@@ -1,9 +0,0 @@
-import { defineAuth } from '@aws-amplify/backend';
-
-export const auth = defineAuth({
-	loginWith: {
-		email: {
-			verificationEmailSubject: 'Welcome 👋 Verify your email!'
-		}
-	}
-});
