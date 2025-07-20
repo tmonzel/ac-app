841c70cd44a9996bd48fe1986626a3d98a7de441
Thomas Monzel <t.monzel@gds-gmbh.de>
2025-07-20 09:21:26 +0200

    Remove jobs link

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -3,7 +3,7 @@
 		<img src="favicon.svg" class="w-8 saturate-0 opacity-50" alt="Azubi Companion Logo" />
 	</div>
 	<div class="flex flex-col md:flex-row justify-center gap-1 md:gap-5 lead-font text-center">
-		<a href="jobs" target="_blank">Jobs</a> <span class="hidden md:inline">/</span>
+		<!-- <a href="jobs" target="_blank">Jobs</a> <span class="hidden md:inline">/</span> -->
 		<a href="datenschutz" target="_blank">Datenschutz</a> <span class="hidden md:inline">/</span>
 		<a href="impressum" target="_blank">Impressum</a>
 	</div>
